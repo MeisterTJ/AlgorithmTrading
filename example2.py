3ffdc487da79770b7e12b2a0d83bf95f3137a753
Meister TJ <74967583+MeisterTJ@users.noreply.github.com>
2021-04-29 22:17:37 +0900

    example2 commit

--- example2.py
+++ example2.py
@@ -1,1 +1,9 @@
-wefwe
+import pybithumb
+import time
+
+# 빗썸 모든 가상화폐 티커와 가격을 0.1초 마다 하나씩 출력
+tickers = pybithumb.get_tickers()
+for ticker in tickers:
+    price = pybithumb.get_current_price(ticker)
+    print(ticker, price)
+    time.sleep(0.1)
